b5c1fdeac2239ad78096dbca1ddec02cf3bf549f
AvidCoder27 <gilireeves@gmail.com>
2024-12-21 10:50:50 -0500

    fix photonvision

--- src/main/java/frc/team5115/subsystems/vision/PhotonVision.java
+++ src/main/java/frc/team5115/subsystems/vision/PhotonVision.java
@@ -1,17 +1,16 @@
 package frc.team5115.subsystems.vision;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.team5115.Constants.VisionConstants;
-import frc.team5115.subsystems.drive.Drivetrain;
-import java.util.Optional;
 import org.littletonrobotics.junction.Logger;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.team5115.Constants.VisionConstants;
+import frc.team5115.subsystems.drive.Drivetrain;
 
 public class PhotonVision extends SubsystemBase {
     private final Drivetrain drivetrain;
@@ -22,24 +21,25 @@ public class PhotonVision extends SubsystemBase {
     public PhotonVision(Drivetrain drivetrain) {
         this.drivetrain = drivetrain;
         camera = new PhotonCamera(VisionConstants.cameraName);
-        fieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
-        poseEstimator =
-                new PhotonPoseEstimator(
-                        fieldLayout, PoseStrategy.LOWEST_AMBIGUITY, camera, VisionConstants.robotToCam);
-    }
-
-    public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
-        poseEstimator.setReferencePose(prevEstimatedRobotPose);
-        var pose = poseEstimator.update();
-        return pose;
+        fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo);
+        poseEstimator = new PhotonPoseEstimator(fieldLayout, PoseStrategy.LOWEST_AMBIGUITY, VisionConstants.robotToCam);
     }
 
     @Override
     public void periodic() {
-        var option = getEstimatedGlobalPose(drivetrain.getPose());
-        Logger.recordOutput("Vision/HasMeasurement", option.isPresent());
-        if (option.isPresent()) {
-            EstimatedRobotPose pose = option.get();
+        poseEstimator.setReferencePose(drivetrain.getPose());
+        final var unread = camera.getAllUnreadResults();
+        EstimatedRobotPose pose = null;
+        for (final var result : unread) {
+            final var option = poseEstimator.update(result);
+            if (option.isPresent()) {
+                pose = option.get();
+            }
+        }
+
+        final boolean hasMeasurement = pose != null;
+        Logger.recordOutput("Vision/HasMeasurement", hasMeasurement);
+        if (hasMeasurement) {
             drivetrain.addVisionMeasurement(pose.estimatedPose.toPose2d(), pose.timestampSeconds);
             Logger.recordOutput("Vision/EstimatedPose", pose.estimatedPose);
         }
