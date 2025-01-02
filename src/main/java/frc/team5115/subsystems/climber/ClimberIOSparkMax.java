00a309ed8afe1162ec0fb73479bc92da0adf4f2d
Idiakosa <kosa3200@gmail.com>
2025-01-02 15:55:21 -0500

    upd. climber subsystem for 2025

--- src/main/java/frc/team5115/subsystems/climber/ClimberIOSparkMax.java
+++ src/main/java/frc/team5115/subsystems/climber/ClimberIOSparkMax.java
@@ -1,40 +1,37 @@
 package frc.team5115.subsystems.climber;
 
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.team5115.Constants;
 
 public class ClimberIOSparkMax implements ClimberIO {
-    public final CANSparkMax leftClimb;
-    public final CANSparkMax rightClimb;
+    public final SparkMax leftClimb;
+    public final SparkMax rightClimb;
     public final RelativeEncoder leftClimbEncoder;
     public final RelativeEncoder rightClimbEncoder;
 
     public ClimberIOSparkMax() {
-        leftClimb = new CANSparkMax(Constants.CLIMBER_LEFT_MOTOR_ID, MotorType.kBrushless);
-        leftClimb.setInverted(true);
-        leftClimb.setIdleMode(IdleMode.kBrake);
-        leftClimb.setSmartCurrentLimit(45);
+        leftClimb = new SparkMax(Constants.CLIMBER_LEFT_MOTOR_ID, MotorType.kBrushless);
+        SparkMaxConfig leftMotorConfig = new SparkMaxConfig();
+        leftMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(45).inverted(true);
         leftClimbEncoder = leftClimb.getEncoder();
-        leftClimbEncoder.setPositionConversionFactor(1.0 / 16.0);
 
-        rightClimb = new CANSparkMax(Constants.CLIMBER_RIGHT_MOTOR_ID, MotorType.kBrushless);
-        rightClimb.setInverted(false);
-        rightClimb.setIdleMode(IdleMode.kBrake);
-        rightClimb.setSmartCurrentLimit(45);
+        rightClimb = new SparkMax(Constants.CLIMBER_RIGHT_MOTOR_ID, MotorType.kBrushless);
+        SparkMaxConfig rightMotorConfig = new SparkMaxConfig();
+        rightMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(45).inverted(false);
         rightClimbEncoder = rightClimb.getEncoder();
-        rightClimbEncoder.setPositionConversionFactor(1.0 / 16.0);
     }
 
     @Override
     public void updateInputs(ClimberIOInputs inputs) {
-        inputs.leftAngle = Rotation2d.fromRotations(leftClimbEncoder.getPosition());
-        inputs.rightAngle = Rotation2d.fromRotations(rightClimbEncoder.getPosition());
-        inputs.leftVelocityRPM = leftClimbEncoder.getVelocity();
-        inputs.rightVelocityRPM = rightClimbEncoder.getVelocity();
+        inputs.leftAngle = Rotation2d.fromRotations(leftClimbEncoder.getPosition() / 16.0);
+        inputs.rightAngle = Rotation2d.fromRotations(rightClimbEncoder.getPosition() / 16.0);
+        inputs.leftVelocityRPM = leftClimbEncoder.getVelocity() / 16.0;
+        inputs.rightVelocityRPM = rightClimbEncoder.getVelocity() / 16.0;
         inputs.leftCurrentAmps = leftClimb.getOutputCurrent();
         inputs.rightCurrentAmps = rightClimb.getOutputCurrent();
         inputs.leftAppliedVolts = leftClimb.getAppliedOutput();
