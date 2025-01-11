65c52fb8ec5eb78611aeb6e43ff48ca98833c749
Idiakosa <kosa3200@gmail.com>
2025-01-11 14:25:19 -0500

    it works

--- src/main/java/frc/team5115/RobotContainer.java
+++ src/main/java/frc/team5115/RobotContainer.java
@@ -2,8 +2,7 @@ package frc.team5115;
 
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj2.command.Command;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+import java.util.ArrayList;
 import org.littletonrobotics.junction.Logger;
 
 /**
@@ -14,6 +13,9 @@ import org.littletonrobotics.junction.Logger;
  */
 public class RobotContainer {
     private final SerialPort port;
+    private final ArrayList<Character> queue = new ArrayList<Character>();
+    private int distance;
+    private boolean hasMeasurement;
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
@@ -21,14 +23,31 @@ public class RobotContainer {
     }
 
     public void robotPeriodic() {
-        final byte[] byteArray = port.read(2);
-        Logger.recordOutput("Raw Bytes", byteArray);
-
-        final ByteBuffer bb = ByteBuffer.allocate(2);
-        bb.order(ByteOrder.BIG_ENDIAN);
-        bb.put(byteArray);
-        final short value = bb.getShort(0);
-        Logger.recordOutput("Short", value);
+        while (port.getBytesReceived() > 0) {
+            final char nextChar = (char) port.read(1)[0];
+            if (nextChar == '\n') {
+                final StringBuilder builder = new StringBuilder();
+                queue.forEach((ch) -> builder.append(ch));
+                queue.clear();
+                final String str = builder.toString().trim();
+                int num = -1;
+                try {
+                    num = Integer.parseInt(str, 10);
+                } catch (NumberFormatException e) {
+                    System.out.print("ParseInt error: ");
+                    System.out.println(e);
+                }
+                if (hasMeasurement = (num != -1)) {
+                    distance = num;
+                }
+                Logger.recordOutput("String", str);
+            } else {
+                queue.add(nextChar);
+            }
+        }
+        Logger.recordOutput("QSize", queue.size());
+        Logger.recordOutput("Distance (mm)", distance);
+        Logger.recordOutput("Has Measurement", hasMeasurement);
     }
 
     public Command getAutonomousCommand() {
