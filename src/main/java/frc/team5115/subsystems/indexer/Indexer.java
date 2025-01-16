c02ea1fc69d592d51e69558a5a01a7af5f4bd8b1
Idiakosa <kosa3200@gmail.com>
2025-01-16 15:50:23 -0500

    actually made indexer this time

--- src/main/java/frc/team5115/subsystems/indexer/Indexer.java
+++ src/main/java/frc/team5115/subsystems/indexer/Indexer.java
@@ -1,3 +1,33 @@
 package frc.team5115.subsystems.indexer;
 
-public class Indexer {}
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class Indexer extends SubsystemBase {
+    private final IndexerIO io;
+    private final IndexerIOInputsAutoLogged inputs = new IndexerIOInputsAutoLogged();
+
+    public Indexer(IndexerIO io) {
+        this.io = io;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Intake", inputs);
+    }
+
+    public Command setSpeed(double percent) {
+        return Commands.runOnce(() -> io.setPercent(percent), this);
+    }
+
+    public Command index() {
+        return setSpeed(+1);
+    }
+
+    public Command stop() {
+        return setSpeed(+0);
+    }
+}
