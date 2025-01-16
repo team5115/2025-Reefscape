e835d17903ec6a4d2bc63e430353467e242b6c03
AvidCoder27 <107364066+AvidCoder27@users.noreply.github.com>
2025-01-16 16:21:43 -0500

    Indexer (#2)
    * indexer
    
    * indexer subsystem code
    
    * unused imports
    
    * actually made indexer this time
    
    * indexer sensor exsists now
    
    * built
    
    ---------
    
    Co-authored-by: Idiakosa <kosa3200@gmail.com>

---
+++ src/main/java/frc/team5115/subsystems/indexer/IndexerIO.java
@@ -0,0 +1,23 @@
+package frc.team5115.subsystems.indexer;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IndexerIO {
+    @AutoLog
+    public static class IndexerIOInputs {
+        public double velocityRPM = 0.0;
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
+
+        public boolean coralDetected = false;
+    }
+
+    /** Updates the set of loggable inputs. */
+    public default void updateInputs(IndexerIOInputs inputs) {}
+
+    /** Run the intake motor at the specified voltage. */
+    public default void setVoltage(double volts) {}
+
+    /** Run the intake motor at the specified percentage. */
+    public default void setPercent(double percent) {}
+}
---
+++ src/main/java/frc/team5115/subsystems/indexer/IndexerIOSparkMax.java
@@ -0,0 +1,47 @@
+package frc.team5115.subsystems.indexer;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj.DigitalInput;
+import frc.team5115.Constants;
+
+public class IndexerIOSparkMax implements IndexerIO {
+    private final SparkMax motor;
+    private final RelativeEncoder encoder;
+
+    private final DigitalInput sensor;
+
+    public IndexerIOSparkMax() {
+        sensor = new DigitalInput(Constants.INDEXER_SENSOR_ID);
+        motor = new SparkMax(Constants.INDEXER_MOTOR_ID, MotorType.kBrushless);
+        encoder = motor.getEncoder();
+
+        final SparkMaxConfig motorConfig = new SparkMaxConfig();
+        motorConfig.smartCurrentLimit(60, 80).idleMode(IdleMode.kCoast);
+        motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    @Override
+    public void updateInputs(IndexerIOInputs inputs) {
+        inputs.velocityRPM = encoder.getVelocity();
+        inputs.appliedVolts = motor.getAppliedOutput() * motor.getBusVoltage();
+        inputs.currentAmps = motor.getOutputCurrent();
+
+        inputs.coralDetected = !sensor.get();
+    }
+
+    @Override
+    public void setPercent(double percent) {
+        motor.set(percent);
+    }
+
+    @Override
+    public void setVoltage(double volts) {
+        motor.setVoltage(volts);
+    }
+}
