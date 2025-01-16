3495da6bee9783fb983fa2ef568a6bfcbb9d75d6
AvidCoder27 <107364066+AvidCoder27@users.noreply.github.com>
2025-01-16 16:20:08 -0500

    dispenser (#3)

---
+++ src/main/java/frc/team5115/subsystems/dispenser/Dispenser.java
@@ -0,0 +1,41 @@
+package frc.team5115.subsystems.dispenser;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class Dispenser extends SubsystemBase {
+    private final DispenserIO io;
+    private final DispenserIOInputsAutoLogged inputs = new DispenserIOInputsAutoLogged();
+
+    public Dispenser(DispenserIO io) {
+        this.io = io;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Dispense", inputs);
+    }
+
+    public Command waitForDetectionState(boolean state, double timeout) {
+        return Commands.waitUntil(() -> inputs.coralDetected == state).withTimeout(timeout);
+    }
+
+    public Command setSpeed(double percent) {
+        return Commands.runOnce(() -> io.setPercent(percent), this);
+    }
+
+    public Command dispense() {
+        return setSpeed(+1);
+    }
+
+    public Command stop() {
+        return setSpeed(0);
+    }
+
+    public boolean coralDetected() {
+        return inputs.coralDetected;
+    }
+}
---
+++ src/main/java/frc/team5115/subsystems/dispenser/DispenserIOSparkMax.java
@@ -0,0 +1,47 @@
+package frc.team5115.subsystems.dispenser;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj.DigitalInput;
+import frc.team5115.Constants;
+
+public class DispenserIOSparkMax implements DispenserIO {
+    private final SparkMax motor;
+    private final RelativeEncoder encoder;
+
+    private final DigitalInput sensor;
+
+    public DispenserIOSparkMax() {
+        sensor = new DigitalInput(Constants.DISPENSER_SENSOR_ID);
+        motor = new SparkMax(Constants.DISPENSER_MOTOR_ID, MotorType.kBrushless);
+        encoder = motor.getEncoder();
+
+        final SparkMaxConfig motorConfig = new SparkMaxConfig();
+        motorConfig.smartCurrentLimit(60, 80).idleMode(IdleMode.kCoast);
+        motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    @Override
+    public void updateInputs(DispenserIOInputs inputs) {
+        inputs.velocityRPM = encoder.getVelocity();
+        inputs.appliedVolts = motor.getAppliedOutput() * motor.getBusVoltage();
+        inputs.currentAmps = motor.getOutputCurrent();
+
+        inputs.coralDetected = !sensor.get();
+    }
+
+    @Override
+    public void setPercent(double percent) {
+        motor.set(percent);
+    }
+
+    @Override
+    public void setVoltage(double volts) {
+        motor.setVoltage(volts);
+    }
+}
