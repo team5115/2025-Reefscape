d7dbf9127748bee649cc9ea3dd01fa5038533c2c
avidcoder27 <gilireeves@gmail.com>
2025-01-23 18:02:22 -0500

    update cam name

--- src/main/java/frc/team5115/Constants.java
+++ src/main/java/frc/team5115/Constants.java
@@ -103,7 +103,7 @@ public final class Constants {
     }
 
     public static class VisionConstants {
-        public static final String cameraName = "FOV_120_Degrees";
+        public static final String cameraName = "USB_GS_Camera";
         public static final double camYaw = Math.toRadians(0);
         public static final double camPitch = Math.toRadians(0);
         public static final double camRoll = Math.toRadians(180);
