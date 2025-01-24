81c8708772eaf3d990ee94e57ba3a42aec990611
Idiakosa <kosa3200@gmail.com>
2025-01-24 15:52:33 -0500

    change wheel cof and max linear speed

--- src/main/java/frc/team5115/Constants.java
+++ src/main/java/frc/team5115/Constants.java
@@ -56,8 +56,8 @@ public final class Constants {
         // ! TODO determine mass and moi of robot, and wheel cof
         public static final Mass ROBOT_MASS = Pounds.of(70);
         public static final MomentOfInertia ROBOT_MOI = KilogramSquareMeters.of(6);
-        public static final double WHEEL_COF = 1.55;
-        public static final double MAX_LINEAR_SPEED = 4.8; // meters per second
+        public static final double WHEEL_COF = 1.5;
+        public static final double MAX_LINEAR_SPEED = 4.2; // meters per second
         public static final double TRACK_WIDTH_X =
                 Units.inchesToMeters(23.75); // TODO confirm track width
         public static final double TRACK_WIDTH_Y = Units.inchesToMeters(23.75);
