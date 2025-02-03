555d19426fce97d2bef8f5c7326a63e821219128
avidcoder27 <gilireeves@gmail.com>
2025-02-03 15:14:17 -0500

    begun the implamentation of the dealgaeficationinator5000

---
+++ src/main/java/frc/team5115/subsystems/dealgaefacationinator5000/Dealgaefacationinator5000IO.java
@@ -0,0 +1,12 @@
+package frc.team5115.subsystems.dealgaefacationinator5000;
+
+public interface Dealgaefacationinator5000IO {
+
+    public static class Dealgaefacationinator5000IOInputs{
+
+    }
+
+    public default void updateInputs(Dealgaefacationinator5000IOInputs inputs){}
+
+    public default void setVoltage(double volts){}
+}
