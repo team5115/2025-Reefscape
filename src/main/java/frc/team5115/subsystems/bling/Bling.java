7a6775a3aef3ca8d9e162492b76cbf071eb75668
Idiakosa <kosa3200@gmail.com>
2025-02-04 17:22:50 -0500

    knight rider!

--- src/main/java/frc/team5115/subsystems/bling/Bling.java
+++ src/main/java/frc/team5115/subsystems/bling/Bling.java
@@ -2,29 +2,37 @@ package frc.team5115.subsystems.bling;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
 
 public class Bling extends SubsystemBase {
     private final AddressableLED led;
     private final AddressableLEDBuffer ledBuffer;
+    private final double[] prevPercent;
     private final byte[][] buffer; // GRB
+    private final int ledCount;
+
+    private final int period = 1;
+    private final double tailLength = 20;
+    private final double decay = 1d / tailLength;
+    private final int minPower = 10;
+    private final int maxPower = 255;
+
+    private int timer_idle;
+    private int counter_idle;
+    private int direction_idle;
 
     public Bling() {
+        ledCount = 144;
         led = new AddressableLED(0);
-        ledBuffer = new AddressableLEDBuffer(192);
-
+        ledBuffer = new AddressableLEDBuffer(ledCount * 4 / 3);
         buffer = new byte[ledBuffer.getLength()][3];
-
+        prevPercent = new double[ledCount];
         led.setLength(ledBuffer.getLength());
 
-        setRGBW(3, 100, 0, 0, 0);
-        setRGBW(4, 0, 100, 0, 0);
-        setRGBW(5, 0, 0, 100, 0);
-        setRGBW(6, 0, 0, 0, 100);
-        loadData();
-        led.start();
+        timer_idle = 0;
+        counter_idle = 0;
+        direction_idle = 1;
     }
 
     public void setRGBW(int i, byte r, byte g, byte b, byte w) {
@@ -49,9 +57,36 @@ public class Bling extends SubsystemBase {
         led.setData(ledBuffer);
     }
 
-    public Command knightRider(){
-        return Commands.run(() -> {
-            
-        });
+    @Override
+    public void periodic() {
+        Logger.recordOutput("Bling/TimerIdle", timer_idle);
+        Logger.recordOutput("Bling/CounterIdle", counter_idle);
+        Logger.recordOutput("Bling/DirectionIdle", direction_idle);
+
+        timer_idle++;
+        if (timer_idle >= period) {
+            timer_idle = 0;
+        } else {
+            return;
+        }
+
+        counter_idle += direction_idle * 2;
+        if (counter_idle >= ledCount || counter_idle < 0) {
+            direction_idle = -direction_idle;
+            counter_idle = direction_idle > 0 ? 0 : ledCount - 1;
+        }
+
+        for (int i = 0; i < ledCount; i++) {
+            double percent = prevPercent[i] - decay;
+            percent = Math.max(percent, 0);
+            if (i == counter_idle || i == counter_idle - direction_idle) {
+                percent = 1.0;
+            }
+            prevPercent[i] = percent;
+            final double power = (percent * (maxPower - minPower)) + minPower;
+            setRGBW(i, (int) power, 0, 0, 0);
+        }
+        loadData();
+        led.start();
     }
 }
