16b20206584141bf3a29ddf33056a0b9d53b7c14
avidcoder27 <gilireeves@gmail.com>
2025-03-08 18:58:44 -0500

    make left bumper L1, right bumper extends climb

--- src/main/java/frc/team5115/RobotContainer.java
+++ src/main/java/frc/team5115/RobotContainer.java
@@ -231,10 +231,11 @@ public class RobotContainer {
         //         .onFalse(dealgaefacationinator5000.retract());
         joyManip.rightTrigger().onTrue(dispenser.dispense()).onFalse(dispenser.stop());
         joyManip.leftTrigger().onTrue(dispenser.reverse()).onFalse(dispenser.stop());
-        // joyManip.leftBumper().onTrue(climber.retract());
-        // joyManip.rightBumper().onTrue(climber.extend());
+        // joyManip.leftBumper().onTrue();
+        joyManip.rightBumper().onTrue(climber.extend());
+        joyManip.pov(90).onTrue(climber.retract());
         joyManip
-                .rightBumper()
+                .leftBumper()
                 .onTrue(elevator.setHeight(Height.L1))
                 .onFalse(elevator.setHeight(Height.INTAKE));
     }
