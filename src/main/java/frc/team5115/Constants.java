ba2d6e61d28734f6ec03eddada47aafce6ec5b37
avidcoder27 <gilireeves@gmail.com>
2025-03-14 16:56:10 -0400

    update elevator sim constants

--- src/main/java/frc/team5115/Constants.java
+++ src/main/java/frc/team5115/Constants.java
@@ -14,6 +14,8 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Mass;
 import edu.wpi.first.units.measure.MomentOfInertia;
 import edu.wpi.first.wpilibj.RobotBase;
+import frc.team5115.subsystems.elevator.Elevator;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -112,8 +114,8 @@ public final class Constants {
 
     public static class ElevatorConstants {
         // Heights and radius in meters
-        public static final double MAX_HEIGHT = 0.3746;
-        public static final double MIN_HEIGHT = 0;
+        public static final double MAX_HEIGHT = Elevator.Height.L4.position;
+        public static final double MIN_HEIGHT = Elevator.Height.MINIMUM.position;
         public static final double DRUM_RADIUS = 0.05;
         public static final double CARRIAGE_MASS_KG = 9.072; // 20 lbs
         public static final double GEARING = 20.0; // numbers greater than 1 represent reductions
