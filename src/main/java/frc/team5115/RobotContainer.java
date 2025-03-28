2ddfc118b79b56821775cf9c57284c06a3f5f076
AvidCoder27 <107364066+AvidCoder27@users.noreply.github.com>
2025-03-28 08:52:01 -0400

    ;;;;

--- src/main/java/frc/team5115/RobotContainer.java
+++ src/main/java/frc/team5115/RobotContainer.java
@@ -430,7 +430,7 @@ public class RobotContainer {
 
     public void autoInit() {
         drivetrain.setAutoCurrentLimit();
-        drivetrain.offsetGyro(Rotation2d.k180deg)
+        drivetrain.offsetGyro(Rotation2d.k180deg);
     }
 
     public void disabledPeriodic() {}
