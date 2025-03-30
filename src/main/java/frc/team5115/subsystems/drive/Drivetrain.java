75a8bbf406d9943fb35c205ca1c2194da1c3ab51
AvidCoder27 <gilireeves@gmail.com>
2025-03-30 11:45:52 -0400

    increase drivetrain.linear_ki back to 0.5 from 0.25 (it worked on 0.5)

--- src/main/java/frc/team5115/subsystems/drive/Drivetrain.java
+++ src/main/java/frc/team5115/subsystems/drive/Drivetrain.java
@@ -43,7 +43,7 @@ public class Drivetrain extends SubsystemBase {
 
     // TODO tune drive pids
     private final double linear_kp = 1.9;
-    private final double linear_ki = 0.25;
+    private final double linear_ki = 0.5;
     private final double linear_kd = 0.0;
     private final double angular_kp = 0.5;
     private final double angular_ki = 0.0;
